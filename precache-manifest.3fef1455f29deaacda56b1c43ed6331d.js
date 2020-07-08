ee7154f5563ab954809c804dbcaf6720ef5910a3
Ernest Lian <ernest.lian97@hotmail.com>
2020-07-08 12:16:09 -0400

    Updates

---
+++ precache-manifest.3fef1455f29deaacda56b1c43ed6331d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34f1c333e1ba3364eb502ed140c7e13f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "17f537ad034dd970eb62",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "793057d632f020153130",
+    "url": "/static/js/2.75699a50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75699a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f537ad034dd970eb62",
+    "url": "/static/js/main.c2b2d355.chunk.js"
+  },
+  {
+    "revision": "448672934d8e1ad8bff2",
+    "url": "/static/js/runtime-main.e4492a50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
