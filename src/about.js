1c4c99b29965f63c6bb682f32ded7da87d8f48a1
Ernest Lian <ernest.lian97@hotmail.com>
2021-01-20 23:44:21 -0500

    Refactor

--- src/about.js
+++ src/about.js
@@ -28,16 +28,6 @@ const About = () => {
             pt={4}
         >
             <Box
-                component={Typography}
-                variant='h2'
-                color='white'
-                className={` ${classes.halfBackground} ${classes.fontWeight}`}
-                display='flex'
-                justifyContent='center'
-            >
-                about
-            </Box>
-            <Box
                 className='about-container'
                 display='flex'
                 flexDirection='column'
@@ -47,73 +37,32 @@ const About = () => {
                 <Box
                     component={Typography}
                     color='black'
-                    style= {{ 'font-size': '20px'}}
+                    style= {{
+                        'font-size': '20px',
+                        'font-weight': '900'
+                    }}
                     pb={3}
                 >
-                    I am an upcoming Computer Science grad from the University of Waterloo and will be working on the Online Store Editor team at Shopify.
+                    I am ernest, an aspiring Computer Scientist attending the University of Waterloo.
                 </Box>
                 <Box
                     className='about-section-two'
                     component={Typography}
                     color='black'
-                    style= {{ 'font-size': '20px'}}
+                    style= {{ 'font-size': '12px'}}
                     pb={3}
                 >
-                    I have always had an interest in software development as it challenges not only my ability to think from
-                    a critical perspective, but also my creativity. I am interested in writing more accessible
+                    I have 2+ years of experience working across various software positions; QA, back-end development, full stack development. I have a huge interest in front-end development and am passionate about writing more accessible code.
                     code for all individuals.
                 </Box>
                 <Box
                     className='about-section-three'
                     component={Typography}
                     color='black'
-                    style= {{ 'font-size': '20px'}}
+                    style= {{ 'font-size': '12px'}}
                     pb={2}
                 >
-                    I love collecting LP records 💿, an aspiring vegetarian 🌱, and mediocre embroiderer 
-                </Box>
-                <Box
-                display='flex'
-                justifyContent='center'
-                flexDirection='column'
-                className='about-profile-container'
-                pt={5}
-                >
-                    <Box
-                        display='flex'
-                        className='about-images'
-                    >
-                        <Box
-                            className='about-profile'
-                            component='img'
-                            src={ErnestAbout}
-                            pb={3}
-                            pr={2}
-                        />
-                        <Box
-                            className='about-profile-two'
-                            component='img'
-                            src={ErnestAboutSecond}
-                            pb={3}
-                            pl={2}
-                        />
-                    </Box>
-                    <Box
-                        className='about-profile-me'
-                        width='25%'
-                        height='25%'
-                        component='img'
-                        src={Me}
-                    />
-                </Box>
-                <Box
-                    className='about-section-two'
-                    component={Typography}
-                    color='white'
-                    style= {{ 'font-size': '20px'}}
-                    pb={3}
-                >
-                    Check out my top 5 songs
+                    Aside from work, I love collecting LP records, am an aspiring vegetarian, and mediocre embroiderer 
                 </Box>
             </Box>
         </Box>
