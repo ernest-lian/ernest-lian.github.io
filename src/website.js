b02553b43ff947a197b48151fcb7c0044c4a281b
Ernest Lian <ernest.lian97@hotmail.com>
2021-04-19 00:51:27 -0400

    Refactor

--- src/website.js
+++ src/website.js
@@ -66,7 +66,7 @@ const Website = () => {
                     >
                         <Tooltip title="LinkedIn">
                             <Box
-                                // className='social-icons'
+                                className='social-icons'
                             >
                                 <a href='https://www.linkedin.com/in/ernest-lian/' rel="noopener noreferrer" target='_blank'>
                                     <FontAwesomeIcon icon={faLinkedin} size='2x' color='white'/>
@@ -75,7 +75,7 @@ const Website = () => {
                         </Tooltip>
                         <Tooltip title='Github'>
                             <Box
-                                // className='social-icons'
+                                className='social-icons'
                             >
                                 <a href='https://github.com/ernest-lian' rel="noopener noreferrer" target="_blank">
                                     <FontAwesomeIcon icon={faGithub} size='2x' color='white'/>
@@ -84,7 +84,7 @@ const Website = () => {
                         </Tooltip>
                         <Tooltip title='Email'>
                             <Box
-                                // className='social-icons'
+                                className='social-icons'
                             >
                                 <a href='mailto: ernestclian@gmail.com'>
                                     <FontAwesomeIcon icon={faEnvelope} size='2x' color='white'/>
