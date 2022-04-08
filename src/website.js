d737e24f2336f57cb07439561597bbcd55bbcd73
Ernest Lian <ernest.lian97@hotmail.com>
2022-04-07 23:08:30 -0400

    Fix typo

--- src/website.js
+++ src/website.js
@@ -62,7 +62,7 @@ const Website = () => {
                             'padding-top': '2rem'
                         }}
                     >
-                        Currently enahcing the Theme Editor experience @ <a href="https://www.shopify.ca/" target="_blank" rel="noopener noreferrer" style={{'color': '#95BF47', 'text-decoration': 'none'}}>Shopify</a>
+                        Currently enhancing the Theme Editor experience @ <a href="https://www.shopify.ca/" target="_blank" rel="noopener noreferrer" style={{'color': '#95BF47', 'text-decoration': 'none'}}>Shopify</a>
                     </Box>
                 </Box>
             </Box>
